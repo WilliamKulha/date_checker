564d1882fe63cafca7c244b0783f041334c3fa34
William Kulha <kulha.william@gmail.com>
2018-11-28 11:42:44 -0500

    Add and fail spec that will test function that converts javascript's day number to the day of the week as a string.

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,8 +1,23 @@
+import {convertDay} from './../src/module.js';
 
 describe('getting date', function() {
+  let testDate;
+  let testDateDay;
+
+  beforeEach(function() {
+    testDate = new Date(1321, 11, 12);
+  });
+
+  beforeEach(function() {
+    testDateDay = testDate.getDay();
+  });
 
   it('should return the day of the specified date', function() {
-    let testDate = new Date(1321, 11, 12);
     expect(testDate.getDay()).toEqual(5);
   });
+
+  it('should return a string corresponding to the day of the week that Javascript returns as a number', function() {
+    expect(convertDay(testDateDay)).toEqual('Friday');
+  });
+
 });
