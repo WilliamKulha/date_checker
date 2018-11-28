6fc70dd2170b7063366350d4deabf588ea7bdba2
William Kulha <kulha.william@gmail.com>
2018-11-28 12:14:58 -0500

    Add UI javascript to collect user input, perform convertDay, and display the results for the user.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+import {convertDay} from './../src/module.js'; 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 
 $(function() {
-  console.log('Hey there');
+  $('#user-input')
+    .submit(function(event) {
+      event.preventDefault();
+      let userDate = new Date($('#user-date').val());
+      let converted = convertDay(userDate.getDay());
+      $('#results').show();
+      $('#the-form').hide();
+      $('.user-input-date-here').text($('#user-date').val());
+      $('.user-input-weekday').text(converted);
+    });
 });
